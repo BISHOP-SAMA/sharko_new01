e778557d2cc8ff62d2be3341b0e7a34afbe3e915
BISHOP-SAMA <realbishop001@gmail.com>
2026-03-07 17:29:05 +0100

    Update SHACKToken.sol

--- contract/SHACKToken.sol
+++ contract/SHACKToken.sol
@@ -4,11 +4,21 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SHACKToken is ERC20, Ownable {
+contract xSHACKToken is ERC20, Ownable {
     mapping(address => bool) public minters;
     
-    constructor() ERC20("SHACKO Staking Token", "ASS") Ownable(msg.sender) {}
+    constructor() ERC20("SHACKO XP", "xSHACK") Ownable(msg.sender) {}
     
+    // NON-TRANSFERABLE (users can't sell/trade it)
+    function transfer(address, uint256) public pure override returns (bool) {
+        revert("xSHACK: non-transferable");
+    }
+    
+    function transferFrom(address, address, uint256) public pure override returns (bool) {
+        revert("xSHACK: non-transferable");
+    }
+    
+    // Only staking contract can mint rewards
     function addMinter(address _minter) external onlyOwner {
         minters[_minter] = true;
     }
@@ -22,6 +32,7 @@ contract SHACKToken is ERC20, Ownable {
         _mint(to, amount);
     }
     
+    // Users can burn their own xSHACK (for future converter)
     function burn(uint256 amount) external {
         _burn(msg.sender, amount);
     }
