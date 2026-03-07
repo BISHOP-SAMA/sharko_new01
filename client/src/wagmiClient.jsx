95892114aa65e87709cc077e630a5301fbec7605
BISHOP-SAMA <realbishop001@gmail.com>
2026-03-07 17:38:32 +0100

    Update wagmiClient.jsx

--- client/src/wagmiClient.jsx
+++ client/src/wagmiClient.jsx
@@ -4,7 +4,7 @@ import { WagmiProvider } from 'wagmi';
 import { base, baseSepolia } from 'wagmi/chains';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
-// Configure wagmi
+// Configure wagmi for Base
 export const config = getDefaultConfig({
   appName: 'SHACKO Staking',
   projectId: '5393dc3f086ed3291abbe5c3a7901d04',
