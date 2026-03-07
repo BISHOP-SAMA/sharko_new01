17f6e4ae61f7ce14601b3212770c69d89faf2bda
BISHOP-SAMA <realbishop001@gmail.com>
2026-03-07 12:53:10 +0100

    Update xSHACKToken.sol

--- contract/xSHACKToken.sol
+++ contract/xSHACKToken.sol
@@ -5,43 +5,51 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract xSHACKToken is ERC20, Ownable {
+    uint256 public constant MAX_SUPPLY = 100_000_000 * 1e18;
+
     mapping(address => bool) public minters;
     mapping(address => bool) public burners;
-    
+
     constructor() ERC20("SHACKO XP Token", "xSHACK") Ownable(msg.sender) {}
-    
-    // Override transfer functions to make non-transferable
+
+    // Non-transferable
     function transfer(address, uint256) public pure override returns (bool) {
         revert("xSHACK: token is non-transferable");
     }
-    
+
     function transferFrom(address, address, uint256) public pure override returns (bool) {
         revert("xSHACK: token is non-transferable");
     }
-    
-    // Allow minting by authorized contracts (staking)
+
+    function approve(address, uint256) public pure override returns (bool) {
+        revert("xSHACK: approvals disabled");
+    }
+
+    // Minter control
     function addMinter(address _minter) external onlyOwner {
         minters[_minter] = true;
     }
-    
+
     function removeMinter(address _minter) external onlyOwner {
         minters[_minter] = false;
     }
-    
+
     function mint(address to, uint256 amount) external {
         require(minters[msg.sender], "Not authorized to mint");
+        require(totalSupply() + amount <= MAX_SUPPLY, "Max supply reached");
+
         _mint(to, amount);
     }
-    
-    // Allow burning by authorized contracts (converter)
+
+    // Burner control
     function addBurner(address _burner) external onlyOwner {
         burners[_burner] = true;
     }
-    
+
     function removeBurner(address _burner) external onlyOwner {
         burners[_burner] = false;
     }
-    
+
     function burn(address from, uint256 amount) external {
         require(burners[msg.sender] || from == msg.sender, "Not authorized to burn");
         _burn(from, amount);
