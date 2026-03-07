3af00b30a7deb14a0a6cec839b46d7c657de55a9
BISHOP-SAMA <realbishop001@gmail.com>
2026-03-07 08:24:37 +0100

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,18 @@
+// main.tsx
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Wagmi + RainbowKit wrapper
+import { WagmiWrapper } from "./wagmiClient";
+
+const container = document.getElementById("root") as HTMLElement;
+
+createRoot(container).render(
+  <React.StrictMode>
+    <WagmiWrapper>
+      <App />
+    </WagmiWrapper>
+  </React.StrictMode>
+);
