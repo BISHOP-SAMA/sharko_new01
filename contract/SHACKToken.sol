b68324636a9f921913e38ccffe85cf6b1d9ac71c
BISHOP-SAMA <realbishop001@gmail.com>
2026-03-07 07:29:31 +0100

    Update SHACKToken.sol

--- contract/SHACKToken.sol
+++ contract/SHACKToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ASSToken is ERC20, Ownable {
+contract SHACKToken is ERC20, Ownable {
     mapping(address => bool) public minters;
     
     constructor() ERC20("SHACKO Staking Token", "ASS") Ownable(msg.sender) {}
