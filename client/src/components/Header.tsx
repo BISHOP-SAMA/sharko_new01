86711bb5cfa29137814fa5912e2e92f13bb26021
BISHOP-SAMA <realbishop001@gmail.com>
2026-03-08 15:04:02 +0100

    Update Header.tsx

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,14 +5,14 @@ import MobileMenu from "./MobileMenu";
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => setIsOpen((prev) => !prev);
-  const closeMenu = () => setIsOpen(false);
-
   return (
-    <header className="relative w-full">
-      {/* YOU MUST PASS isOpen={isOpen} HERE */}
-      <Navbar onMenuClick={toggleMenu} isOpen={isOpen} />
-      <MobileMenu isOpen={isOpen} onClose={closeMenu} />
-    </header>
+    <>
+      {/* CRITICAL: We MUST pass isOpen={isOpen} here. 
+        If we don't, the Navbar doesn't know what icon to show.
+      */}
+      <Navbar onMenuClick={() => setIsOpen(!isOpen)} isOpen={isOpen} />
+      
+      <MobileMenu isOpen={isOpen} onClose={() => setIsOpen(false)} />
+    </>
   );
 }
