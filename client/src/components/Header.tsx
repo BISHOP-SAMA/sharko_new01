c0c7907bfe1f92d67b1e5d43b249725f12bdc9ec
BISHOP-SAMA <realbishop001@gmail.com>
2026-03-08 15:18:15 +0100

    Update Header.tsx

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -6,13 +6,12 @@ export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <>
-      {/* CRITICAL: We MUST pass isOpen={isOpen} here. 
-        If we don't, the Navbar doesn't know what icon to show.
-      */}
+    <header className="contents">
+      {/* 1. We pass 'isOpen' so the icon can render */}
       <Navbar onMenuClick={() => setIsOpen(!isOpen)} isOpen={isOpen} />
       
+      {/* 2. We pass 'onClose' to the menu so links can close it */}
       <MobileMenu isOpen={isOpen} onClose={() => setIsOpen(false)} />
-    </>
+    </header>
   );
 }
