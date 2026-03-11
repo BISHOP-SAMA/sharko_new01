13d8b46b8d7c6928f6dbbb2b0f96e13f340aab16
BISHOP-SAMA <realbishop001@gmail.com>
2026-03-11 08:58:03 +0100

    Update xSHACKToken.sol

--- contract/xSHACKToken.sol
+++ contract/xSHACKToken.sol
@@ -4,54 +4,82 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/**
+ * @title xSHACK Token
+ * @notice Non-transferable XP token earned through staking SHACKO NFTs
+ * @dev This token cannot be transferred between users - only minted/burned by staking contract
+ */
 contract xSHACKToken is ERC20, Ownable {
-    uint256 public constant MAX_SUPPLY = 100_000_000 * 1e18;
-
-    mapping(address => bool) public minters;
-    mapping(address => bool) public burners;
-
-    constructor() ERC20("SHACKO XP Token", "xSHACK") Ownable(msg.sender) {}
-
-    // Non-transferable
-    function transfer(address, uint256) public pure override returns (bool) {
-        revert("xSHACK: token is non-transferable");
+    
+    // Mapping of authorized minters (staking contracts)
+    mapping(address => bool) public authorizedMinters;
+    
+    // Events
+    event MinterAuthorized(address indexed minter);
+    event MinterRevoked(address indexed minter);
+    
+    constructor() ERC20("SHACKO XP", "xSHACK") Ownable(msg.sender) {
+        // No initial supply - tokens only minted through staking
     }
-
-    function transferFrom(address, address, uint256) public pure override returns (bool) {
-        revert("xSHACK: token is non-transferable");
+    
+    /**
+     * @notice Authorize an address to mint xSHACK tokens (typically the staking contract)
+     * @param minter Address to authorize
+     */
+    function authorizeMinter(address minter) external onlyOwner {
+        require(minter != address(0), "Invalid minter address");
+        authorizedMinters[minter] = true;
+        emit MinterAuthorized(minter);
     }
-
-    function approve(address, uint256) public pure override returns (bool) {
-        revert("xSHACK: approvals disabled");
+    
+    /**
+     * @notice Revoke minter authorization
+     * @param minter Address to revoke
+     */
+    function revokeMinter(address minter) external onlyOwner {
+        authorizedMinters[minter] = false;
+        emit MinterRevoked(minter);
     }
-
-    // Minter control
-    function addMinter(address _minter) external onlyOwner {
-        minters[_minter] = true;
-    }
-
-    function removeMinter(address _minter) external onlyOwner {
-        minters[_minter] = false;
-    }
-
+    
+    /**
+     * @notice Mint xSHACK tokens (only callable by authorized minters)
+     * @param to Address to mint tokens to
+     * @param amount Amount of tokens to mint
+     */
     function mint(address to, uint256 amount) external {
-        require(minters[msg.sender], "Not authorized to mint");
-        require(totalSupply() + amount <= MAX_SUPPLY, "Max supply reached");
-
+        require(authorizedMinters[msg.sender], "Not authorized to mint");
         _mint(to, amount);
     }
-
-    // Burner control
-    function addBurner(address _burner) external onlyOwner {
-        burners[_burner] = true;
-    }
-
-    function removeBurner(address _burner) external onlyOwner {
-        burners[_burner] = false;
-    }
-
+    
+    /**
+     * @notice Burn xSHACK tokens (e.g., when spending on raffles)
+     * @param from Address to burn tokens from
+     * @param amount Amount of tokens to burn
+     */
     function burn(address from, uint256 amount) external {
-        require(burners[msg.sender] || from == msg.sender, "Not authorized to burn");
+        require(authorizedMinters[msg.sender], "Not authorized to burn");
         _burn(from, amount);
+    }
+    
+    /**
+     * @notice Override transfer to make token non-transferable
+     * @dev This prevents users from transferring xSHACK - it can only be earned
+     */
+    function transfer(address, uint256) public pure override returns (bool) {
+        revert("xSHACK is non-transferable");
+    }
+    
+    /**
+     * @notice Override transferFrom to make token non-transferable
+     */
+    function transferFrom(address, address, uint256) public pure override returns (bool) {
+        revert("xSHACK is non-transferable");
+    }
+    
+    /**
+     * @notice Override approve to prevent approvals (since non-transferable)
+     */
+    function approve(address, uint256) public pure override returns (bool) {
+        revert("xSHACK is non-transferable");
     }
 }
