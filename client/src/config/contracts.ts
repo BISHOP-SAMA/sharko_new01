714024917e07eb45bd4df01a453663edd0ed69d8
BISHOP-SAMA <realbishop001@gmail.com>
2026-03-11 23:35:24 +0100

    Create config/contracts.ts

---
+++ client/src/config/contracts.ts
@@ -0,0 +1,17 @@
+// Deployed Contract Addresses - Base Sepolia Testnet
+export const CONTRACTS = {
+  // NFT Contract
+  NFT_ADDRESS: "0x88670d32A2646973895AA1C111d24ee18A585007",
+  
+  // xSHACK Token  
+  XSHACK_ADDRESS: "0xDF90DefD06AFBdD8a80E546677DFe2df66D418dc",
+  
+  // Staking Contract
+  STAKING_ADDRESS: "0x4Bf7a99A9D0583B3bc68Ca9480639B59d1E4A787",
+  
+  // Treasury Wallet
+  TREASURY_ADDRESS: "0xc1e4c27A2E20EA7C6966F6945738171EEeEf5FC7",
+  
+  // Network
+  CHAIN_ID: 84532, // Base Sepolia
+};
